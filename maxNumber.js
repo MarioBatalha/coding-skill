5b16b499616092b12d91693776219a8f408f29f7
MarioBatalha <mariobatalha9@gmail.com>
2021-10-08 13:20:09 -0300

    add minNumber, maxNumber and hooks

---
+++ maxNumber.js
@@ -0,0 +1,3 @@
+const arr =[10, 20, 45, 100, 1, 0, 9, 1000, 2021];
+
+const max = Math.max.apply(Math, arr);
---
+++ minNumber.js
@@ -0,0 +1,5 @@
+const arr = [12, 4, 40, 3, -55, 55];
+
+const min = Math.min.apply(Math, arr);
+
+console.log(min);
