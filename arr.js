2b6511e0c68d885601c6d9d49b5c961fceaadfd5
MarioBatalha <mariobatalha9@gmail.com>
2021-10-22 15:49:04 +0100

    update arr

--- arr.js
+++ arr.js
@@ -1,3 +1,6 @@
-let evens = Array(100).fill(0).map((e, i) => i + 1).filter(e => e % 2 === 0);
+let divisibleByThreeAndFive = Array(100)
+  .fill(0)
+  .map((e, i) => i + 1)
+  .filter((e) => e % 3 === 0 && e % 5 === 0);
 
-console.log(evens);
+//Output: [ 15, 30, 45, 60, 75, 90 ]
