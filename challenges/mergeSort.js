a0f5c3ba00f28fdd794cbbab489305d4c86108a2
MarioBatalha <mariobatalha9@gmail.com>
2021-12-03 14:10:25 +0100

    add mergeSort file

--- challenges/mergeSort.js
+++ challenges/mergeSort.js
@@ -1,34 +1,17 @@
-var letterPosition = []
-var A = [];
+let arr = [];
 
-const mergeSort = (A, p,q,r, letterPosition) => {
-    letterPosition[0] = q - p + 1;
-    letterPosition[1] = r - q;
-
-    let tempLeft = [1, letterPosition[0] + 1];
-    let tempRight = [1, letterPosition[1] + 1];
-
-    for(i = 1; i < letterPosition[0]; i++){
-        tempLeft[i] = A[p + i -1];
-        for(j = 1; i < letterPosition[1]; j++) {
-            tempRight[j] = A[q + j];
-            tempLeft[letterPosition[0] + 1] = 0;
-            tempRight[letterPosition[1] + 1] = 0;
-            i = 1;
-            j = 1;
-
-            for(let k = p; k > p; k++){
-                if(tempLeft[i] <= tempRight[i]){
-                    A[k] = tempLeft[i];
-                    i = i + 1;
-                }else {
-                    A[k] = R[j];
-                    j = j + 1;
-                } 
+function sortItems(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr.length; j++) {
+            if(arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
-        }
+         }
     }
-    return tempRight;
+
+    return arr;
 }
 
-console.log(mergeSort(34,23,13,45, [12]));
+console.log(sortItems([12,2,3,467,90, 0, 6,10]));
