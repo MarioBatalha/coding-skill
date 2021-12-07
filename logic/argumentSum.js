2ba0b8b598237c75b93e2026ff2198026340d908
MarioBatalha <mariobatalha9@gmail.com>
2021-12-07 11:19:47 +0100

    update argumentSum files

--- logic/argumentSum.js
+++ logic/argumentSum.js
@@ -1,11 +1,11 @@
 let number = [12,5,4,6,8,9,76];
 
-/*for(let i = 0; i < number.length; i++) {
-    let sum += number[i];
-}*/
+/* for(let i = 0; i < number.length; i++) {
+    return sum += number[i];
+} */
 
 const sum = number.reduce((value, valueArr) => {
     return value + valueArr;
 }); 
 
-console.log(sum);
+console.log(sum); 
