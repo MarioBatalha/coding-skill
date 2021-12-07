af8e37cf7df2e506d4e18b80f5c910c54f9bfdfd
MarioBatalha <mariobatalha9@gmail.com>
2021-12-07 13:50:09 +0100

    update staircase.js

---
+++ challenges/staircase.js
@@ -0,0 +1,9 @@
+const stairCase = numberOfStairs => {
+    for(i = 0; i <= numberOfStairs; i++) {
+       console.log('#')
+    }
+}
+
+console.log(stairCase(4));
+
+//Thinking in a different logic.   
