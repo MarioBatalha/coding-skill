70e6d125ff50458c2854b11160f741edcb9a65aa
MarioBatalha <mariobatalha9@gmail.com>
2021-12-07 11:48:43 +0100

    add pigLatin files

---
+++ challenges/pigLatin.js
@@ -0,0 +1,16 @@
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+let newStr = "";
+
+const translatePigLatin = str => {
+    if(vowels.indexOf(str[0]) > -1) {
+        newStr = str + 'way';
+        return newStr;
+    } else {
+        let firstMatch = str.match(/[aeiou]/g) || 0;
+        let vowel = str.indexOf(firstMatch[0]);
+        newStr = str.substring(vowel) + str.substring(0, vowel) + 'ay';
+        return newStr;
+    }
+}
+
+console.log(translatePigLatin('pig'))
