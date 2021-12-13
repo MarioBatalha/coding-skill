5bf75936af65ecdf8fc33d186ba5e2710d4e527c
MarioBatalha <mariobatalha9@gmail.com>
2021-12-13 15:51:00 +0100

    add diagonalDiference files

--- challenges/diagonalDiference.js
+++ challenges/diagonalDiference.js
@@ -1,11 +1,20 @@
-const diagonalDiference = (matrix) => {
-    const length = matrix.length;
-    let diagonal1 = 0, diagonal2 = 0;
-
-    for(let i = 0; i < length; i++) {
-        diagonal1 += matrix[i][i];
-        diagonal2 += matrix[length - 1 - i][i]
+const diagonalDifference = (arr) => {
+    var n = arr.length;
+    var diag1 = 0;
+    var diag2 = 0;
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < n; j++){
+            // an element from the main diagonal
+            if(i === j) { 
+                diag1 += arr[i][j];
+            }
+            // an element from the counterdiagonal
+            if(i + j === n - 1){
+                diag2 += arr[i][j];
+            }
+        }
     }
-
-    return Math.abs(diagonal1 - diagonal2);
+    return Math.abs(diag1 - diag2);
 } 
+
+console.log(diagonalDifference([[1,2,3], [4,5,6], [9,8,9]]))
