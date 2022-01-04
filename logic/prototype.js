43c79c3433ce8166eac1d9504b85fb55242ed784
MarioBatalha <mariobatalha9@gmail.com>
2022-01-04 18:42:06 +0100

    change prototype function to a class

--- logic/prototype.js
+++ logic/prototype.js
@@ -1,7 +1,9 @@
-function Students (name, age, level) {
-    this.name = name;
-    this.age = age;
-    this.level = level;
+class Students {
+    constructor(name, age, level) {
+        this.name = name;
+        this.age = age;
+        this.level = level;
+    }
 }
 
 var students1 = new Students("Mario", "23", "graduation");
