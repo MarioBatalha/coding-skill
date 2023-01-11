8922b2e23f06e5ed6395af5c0fcc1120fdeb41c9
MarioBatalha <mariobatalha9@gmail.com>
2023-01-11 09:59:37 +0100

    feat: add react-challenge folder

---
+++ react-challenge/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Book } from './components/book/index';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>React course -John Smilga</h1>
+    </div>
+  )
+}
+
+export default App
